33c7034bb4d0847b8919593fea949314440ce745
skncvo <wm3233@naver.com>
2025-01-23 17:08:43 +0900

    series max, min and statistics

--- Series_import_Ex.py
+++ Series_import_Ex.py
@@ -26,6 +26,17 @@ print(samsung_price.loc['2020-12-15'])
 path_series_samsung = "C:/Users/82108/Desktop/2nd winter/Pandas/Data/samsung_excel.xlsx"
 series1_samsung = pd.read_excel(path_series_samsung, sheet_name= 'Sheet1', index_col= 0).squeeze(True)
 series2_samsung = pd.read_excel(path_series_samsung, sheet_name= 'Sheet2', index_col= 0).squeeze(True)
-
 series_samsung = pd.concat([series1_samsung, series2_samsung], ignore_index = False)
-print(series_samsung)
+
+# series의 고점과 저점
+series_samsung.max()
+series_samsung.min()
+print(series_samsung.index[series_samsung.values == series_samsung.max()]) #argmax 쓰셈
+
+# series의 고점과 저점의 인덱스 찾기
+print(series_samsung.iloc[series_samsung.argmax()]) #값
+print(series_samsung.index[series_samsung.argmax()]) #인덱스
+
+# series의 통계치
+print(series_samsung.describe())
+
