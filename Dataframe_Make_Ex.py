d5d3380169abe2c4a71883af86927859be2ca4b9
skncvo <wm3233@naver.com>
2025-02-02 15:31:33 +0900

    Lottechmical loc,iloc and colunms example

--- Dataframe_Make_Ex.py
+++ Dataframe_Make_Ex.py
@@ -14,6 +14,7 @@ ls_df2 = pd.DataFrame(ls, index=[1,2,3,4,5], columns=["ㅎㅇ"])
 dic = {"a": 101, "b":102, "c":102}
 dic_df = pd.DataFrame(dic, index =[0])
 #print(dic_df)
+
 # dic value를 한 차원에 넣고, 행을 key로 지정
 dic_df2 = pd.DataFrame([dic.values()])
 dic_df2.columns = dic.keys()
@@ -23,4 +24,16 @@ dic_df2.columns = dic.keys()
 path = "C:/Users/82108/Desktop/2nd winter/Pandas/Data/롯데케미칼.csv"
 Lotte = pd.read_csv(path, index_col='Date')
 Lotte_df = pd.DataFrame(Lotte)
-print(Lotte_df)
+#print(Lotte_df)
+
+# DataFrame 행과 열 선택 '[]'갯수를 생각
+# []한 개는 한 열로, [[]]는 한 행으으로 | 10:15, '123':'456'이면 [[]]효과
+# loc['인덱스의 값']
+#print(Lotte_df.loc['2018-01-02'])
+#print(Lotte_df.loc[['2018-01-02']])
+
+# iloc[인덱스숫자 ex 10, 1:100, 0,1,2 ]
+#print(Lotte_df.iloc[[1,2,3]])
+#print(Lotte_df.iloc[10:15])
+
+print(Lotte_df[['Open', 'Close', 'Change']])
